e2ee022aac33f0ae34448b20b8fae642a137b09b
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-02-12 12:47:40 +0530

    user routes product API created

--- user/userroute.js
+++ user/userroute.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const emailSender = require("./emailSender")
+const ViewProduct = require("../admin/ViewProduct");
 
 //email sender API for user
 router.post('/contact', emailSender);
 
-//
+//product listing for product page
+router.get('/viewProduct', ViewProduct);
 
 module.exports = router;
